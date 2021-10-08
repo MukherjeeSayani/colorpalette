84c958134dfee2c2afe8c46b9ca079b1eddc9e18
MukherjeeSayani <91260304+MukherjeeSayani@users.noreply.github.com>
2021-10-08 13:24:16 +0530

    updated
    updated

---
+++ src/app/services/color.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IColor } from '../interfaces/icolor';
+
+@Injectable({
+  providedIn: 'root'
+})
+// Singletone -> let colorService = new ColorService()
+export class ColorService implements OnDestroy {
+  color = new BehaviorSubject<any>(null)
+  constructor(private httpClient : HttpClient) {
+
+   }
+  ngOnDestroy(): void {
+    this.color.unsubscribe();
+  }
+
+  getRandomColor<T>(size: number = 0): Observable<T>{
+    let url = `https://random-data-api.com/api/color/random_color`;
+    const finalUrl = size ? `${url}?size=${size}` : url;
+   return this.httpClient.get<T>(finalUrl);
+  }
+}
+
+
